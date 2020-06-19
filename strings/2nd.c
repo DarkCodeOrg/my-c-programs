3aa0bb157a017688f47e6edd2b0a18a5fadbe598
DarkCodeOrg <davidclapton@protonmail.com>
2020-06-20 00:23:53 +0530

    another program on strings (this one has a bug in it)

--- strings/2nd.c
+++ strings/2nd.c
@@ -5,8 +5,17 @@
 
 int main()
 {
-    char code[30];
-    
-    printf("enter the magic code :");
-    
+    char a[10], b[10];
+    printf("enter the first letter :");
+    fgets(a, sizeof(a), stdin);            // the first argument in the fgets function is the char array where the string would be stored
+                                           // the second argument is the maximum size of input that fgets function wll take
+                                           //the third arg is from where fgets will take input here it is stdin that would take input from user during runtime
+    printf("enter the second letter : ");
+    fgets(b, sizeof(b), stdin);
+
+    int i = strlen(a); int j = strlen(b);
+    strcat(a,b);
+    printf("the length of a is %d and as of b is %d and the final string is %s \n",i,j,a);
+
+    return 0;
 }
